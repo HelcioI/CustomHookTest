a50e9f67e334fbe3006a63a6ae54dd8c21971c2b
Helcio Itiyama <helcio.itiyama-ext@dentsu.com>
2023-12-15 17:57:36 -0300

    fix button disable

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,14 +4,14 @@ import useButton from "./useButton";
 interface ButtonProps {
   label: string;
   onPress: () => void;
-  isDisable: boolean;
+  isDisable: boolean
 }
 
 const Button = ({label, onPress, isDisable}: ButtonProps) => {
   const {styles} = useButton({isDisable})
   
   return (
-    <TouchableOpacity style={styles.button} {...{onPress}}>
+    <TouchableOpacity disabled={isDisable} style={styles.button} {...{onPress}}>
       <Text style={styles.buttonLabel}>{label}</Text>
     </TouchableOpacity>
   )
