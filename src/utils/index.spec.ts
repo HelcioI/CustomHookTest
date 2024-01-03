7aa5aa645fd2f27e7bf6cbb13d39766a99372806
Helcio Itiyama <helcio.itiyama-ext@dentsu.com>
2024-01-03 18:12:28 -0300

    add unit tests to utils functions

---
+++ src/utils/index.spec.ts
@@ -0,0 +1,23 @@
+import { isPasswordValid, isValidEmail } from "."
+
+describe("utils tests", () => {
+  it("should be able to validate a valid email", () => {
+    expect(isValidEmail("john.doe@hotmail.com")).toBeTruthy()
+  })
+
+  it("should not be able to validate a invalid email", () => {
+    expect(isValidEmail("john.doe")).toBeFalsy()
+  })
+
+  it("should not be able to validate a empty email", () => {
+    expect(isValidEmail("")).toBeFalsy()
+  })
+
+  it("should return true if password is longer than or equal to 8 characters", () => {
+    expect(isPasswordValid("12345678")).toBeTruthy()
+  })
+
+  it("should return false if password is lesser than 8 characters", () => {
+    expect(isPasswordValid("1234567")).toBeFalsy()
+  })
+})
